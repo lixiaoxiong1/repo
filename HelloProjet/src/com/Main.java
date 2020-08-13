9bc75a50ad0a3a97014bd9c56e0bc17782af6839
lixiong <1508362780@qq.com>
2020-08-13 16:58:42 +0800

    这是idea提交

---
+++ HelloProjet/src/com/Main.java
@@ -0,0 +1,14 @@
+package com;
+
+import org.junit.Test;
+
+public class Main {
+
+    @Test
+    public void testHelloWrold() {
+
+        System.out.println("1111");
+        System.out.println("HelloWorld!");
+    }
+    
+}
