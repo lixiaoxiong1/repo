31d7f1068fee37ba4b2e2a92b97b355ada5ddea0
lixiaoxiong1 <58162908+lixiaoxiong1@users.noreply.github.com>
2020-08-13 14:43:45 +0800

    A
    A

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -3,11 +3,10 @@ import org.junit.Test;
 public class Main {
 //         你们的爱过去就不在
     @Test
-    dasrfasjnopas kda
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
     }
     
-    
+    AASDASDASDASASDASDASDASDASDASD
     sout fhiaenaisd
 }
